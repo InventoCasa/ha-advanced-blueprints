aa1bc6ab335aeb81f02dbd3fd009effb33d835bd
Maik7 <83724253+Maik7@users.noreply.github.com>
2024-05-08 19:51:57 +0200

    Update pv_excess_control.py
    Fixing Issue #57 
    Avoiding error messages if no automation is send

--- PV_Excess_Control/pyscript/pv_excess_control.py
+++ PV_Excess_Control/pyscript/pv_excess_control.py
@@ -240,7 +240,8 @@ class PvExcessControl:
             # Sanity check
             if not self.sanity_check():
                 return on_time
-                
+            if not PvExcessControl.instances:  
+                return on_time     
             # execute only if this the first instance of the dictionary (avoid two automations acting)
             #log.info(f'{self.log_prefix} I am around.')
             first_item = next(iter(PvExcessControl.instances.values()))

10fcdfe049b7d87fca7769c3e423f5b397480aa3
Maik <83724253+Maik7@users.noreply.github.com>
2024-06-16 23:36:13 +0200

    Update PV_Excess_Control/pyscript/pv_excess_control.py
    Co-authored-by: Carlos Sanchez <carlos@apache.org>

--- PV_Excess_Control/pyscript/pv_excess_control.py
+++ PV_Excess_Control/pyscript/pv_excess_control.py
@@ -345,7 +345,7 @@ class PvExcessControl:
                         else:
                             log.debug(f'{log_prefix} Cannot switch on appliance, because appliance switch interval is not reached '
                                       f'({inst.switch_interval_counter}/{inst.appliance_switch_interval}).')
-                    elif (not switched_off_aplliance_to_switch_on_higher_prioritized_one) and (self.calculate_pwr_reducible(inst.appliance_priority) + avg_excess_power) >= (defined_power if inst.appliance_priority <= 500 else 0):
+                    elif (not switched_off_appliance_to_switch_on_higher_prioritized_one) and (self.calculate_pwr_reducible(inst.appliance_priority) + avg_excess_power) >= (defined_power if inst.appliance_priority <= 500 else 0):
                         # excess power is sufficent by switching off lower prioritized appliance(s)
                         if inst.switch_interval_counter >= inst.appliance_switch_interval:
                             self.switch_on(inst)
